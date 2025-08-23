4e4d07beba507179c54bcbfc1634d21b2968aaec
dev-msk5 <110525758+dev-msk5@users.noreply.github.com>
2025-08-23 11:28:42 +0200

    PDF project update
    Merger added

--- fun_projects/pdf_editor.py
+++ fun_projects/pdf_editor.py
@@ -1,1 +1,39 @@
+import pypdf as pdf
+from tkinter import *
+from tkinter import filedialog, messagebox
 
+
+
+def click():
+    pass
+
+def pdf_merge():    # merger function
+    
+    name=str(input("Save merged PDFs as(without extension): "))
+    
+    to_merge = filedialog.askopenfilenames(
+        title="Select PDFs to merge",
+        filetypes=[("PDF Files", "*.pdf")])
+    
+    if not to_merge:
+        return
+    
+    merger = pdf.PdfWriter()
+    if len(to_merge)>1:
+        for file in to_merge:
+            merger.append(file)
+        merger.write(f"{name}.pdf")
+        merger.close()
+    else: 
+        print("Cannot merge less than 1 PDF")
+
+window=Tk()
+window.geometry("1450x1050") # window size
+window.title("PyPDF Editor")
+window.config(bg="#076790") #background color
+
+button1=Button(window,text='Merge')
+button1.config(command=pdf_merge)
+button1.place(x=0,y=0)
+
+window.mainloop()   # creates window
