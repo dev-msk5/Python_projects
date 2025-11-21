0be8cf27695683521313d67008489deb8cb2dcae
dev-msk5 <110525758+dev-msk5@users.noreply.github.com>
2025-11-21 13:10:19 +0100

    Solution

---
+++ freecodecamp/game_characters.py
@@ -0,0 +1,50 @@
+class GameCharacter:
+    def __init__(self,name):
+        self._name = name
+        self._health = 100
+        self._mana = 50
+        self._level = 1
+
+    @property
+    def name(self):
+        return self._name
+    
+    @property
+    def health(self):
+        return self._health
+    
+    @health.setter
+    def health(self,health):
+        if health >=0 and health <=100:
+            self._health = health
+        elif health >100:
+            self._health = 100
+        else:
+            self.health = 0
+    
+    @property
+    def mana(self):
+        return self._mana
+    
+    @mana.setter
+    def mana(self,mana):
+        if mana >=0 and mana <=50:
+            self._mana = mana
+        elif mana >50:
+            self._mana = 50
+        else:
+            self.mana = 0
+
+    @property
+    def level(self):
+        return self._level
+    
+    def level_up(self):
+        self._level += 1
+        self.health = 100
+        self.mana = 50
+        print(f"{self.name} leveled up to {self.level}!")  
+
+    def __str__(self):
+        return f"Name: {self.name}\nLevel: {self.level}\nHealth: {self.health}\nMana: {self.mana}"
+            
