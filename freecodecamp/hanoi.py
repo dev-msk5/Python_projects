9ce0915b3ec5d2846ab83891cf1514a7fd1450df
dev-msk5 <110525758+dev-msk5@users.noreply.github.com>
2025-12-05 22:40:24 +0100

    Solution

--- freecodecamp/hanoi.py
+++ freecodecamp/hanoi.py
@@ -1,6 +1,19 @@
-def hanoi_solver(disks:int):
-    start = []
+def hanoi_solver(disks: int):
+    start = list(range(disks, 0, -1))
     mid = []
     end = []
-    history += f"{start} {mid} {end}\n"
-    print(history)
+    history = []
+    history.append(f"{start} {mid} {end}")
+
+    def _move(n: int, src, aux, dest) -> None:
+        if n == 0:
+            return
+        _move(n - 1, src, dest, aux)
+        dest.append(src.pop())
+        history.append(f"{start} {mid} {end}")
+        _move(n - 1, aux, src, dest)
+
+    _move(disks, start, mid, end)
+    return "\n".join(history)
+
+print(hanoi_solver(2))
