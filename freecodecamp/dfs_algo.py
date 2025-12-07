78af35a73414e35bd57518469b283b5fd6333560
dev-msk5 <110525758+dev-msk5@users.noreply.github.com>
2025-12-07 21:15:27 +0100

    Update dfs_algo.py

--- freecodecamp/dfs_algo.py
+++ freecodecamp/dfs_algo.py
@@ -5,7 +5,6 @@ def dfs(adj_matrix, node):
     def _dfs(n):
         visited.add(n)
         order.append(n)
-        # iterate neighbors in descending index order
         for index in range(len(adj_matrix[n]) - 1, -1, -1):
             if adj_matrix[n][index] == 1 and index not in visited:
                 _dfs(index)
