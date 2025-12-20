323497613a9e02f540f8eb3c611558926ea3555d
dev-msk5 <110525758+dev-msk5@users.noreply.github.com>
2025-12-20 19:01:52 +0100

    Solution

--- freecodecamp/nqueens.py
+++ freecodecamp/nqueens.py
@@ -1,3 +1,33 @@
-def dfs_n_queens(n:int):
-    if 1 > n:
+def dfs_n_queens(n: int):
+
+    if n < 1:
         return []
+    
+    solutions = []
+    board = [-1] * n
+    columns = set()
+    pos_diag = set()
+    neg_diag = set()
+
+    def backtrack(r):
+        if r == n:
+            solutions.append(board[:])
+            return
+
+        for c in range(n):
+            if c in columns or (r + c) in pos_diag or (r - c) in neg_diag:
+                continue
+
+            columns.add(c)
+            pos_diag.add(r + c)
+            neg_diag.add(r - c)
+            board[r] = c
+
+            backtrack(r + 1)
+
+            columns.remove(c)
+            pos_diag.remove(r + c)
+            neg_diag.remove(r - c)
+
+    backtrack(0)
+    return solutions
